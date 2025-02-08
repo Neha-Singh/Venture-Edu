fe1a0945cfd90cf41e7e0050d448f4cc99638ee5
Neha Singh <nehasngh56@gmail.com>
2025-02-08 14:29:09 +0530

    completed program structure page

--- src/components/ProgramStructure.jsx
+++ src/components/ProgramStructure.jsx
@@ -4,26 +4,32 @@ import { Button } from "@mui/material";
 
 const sprintData = [
   {
-    title: "Foundation building (Month 1 – 2)",
+    title: "Foundation",
+    duration: "Month 1 – 2",
     focus: "Understanding fundamentals and exploring ideas for your venture",
     goal: "Provide participants with a solid foundation of basics needed to build a venture",
     icon: "📚",
   },
   {
-    title: "Startup Immersion (Month 3 – 8)",
+    title: "Immersion",
+    duration: "Month 3 – 8",
+
     focus:
       "Work hands-on with growth stage startups while lightly exploring your venture idea",
     goal: "Develop practical skills and entrepreneurial insight by working directly with founders",
     icon: "🚀",
   },
   {
-    title: "Validation and testing (Month 9 – 10)",
-    focus: "Transition into refining and launching your venture",
+    title: "Validation",
+    duration: "Month 9 – 10",
+    focus:
+      "Transition into refining and launching your venture and launching your venture",
     goal: "Build momentum for your venture through mentorship with user insights",
     icon: "✅",
   },
   {
-    title: "Product development and pitching (Month 11 – 12)",
+    title: "Pitching",
+    duration: "Month 11 – 12",
     focus:
       "Refining your product through mentorship, refining and launching your venture",
     goal: "Fully transition to launching your venture with structured support and resources",
@@ -32,37 +38,50 @@ const sprintData = [
 ];
 
 const ProgramStructure = () => {
-  const [selectedSprint, setSelectedSprint] = useState(null);
+  const [hoveredSprint, setHoveredSprint] = useState(null);
 
   return (
-    <div className="program-structure">
-      <h1 className="title">Program Structure</h1>
+    <div className="program-structure" id="program-structure">
+      <div className="title">Program Structure</div>
       <div className="sprint-buttons">
-        {sprintData.map((sprint, index) => (
-          <Button
+        {sprintData.map((_, index) => (
+          <button
             key={index}
-            variant="outlined"
-            className="arrow-button"
-            onClick={() => setSelectedSprint(sprint)}
+            className={`arrow-button ${
+              hoveredSprint === index ? "highlight" : ""
+            }`}
+            onMouseEnter={() => setHoveredSprint(index)}
+            onMouseLeave={() => setHoveredSprint(null)}
           >
             Sprint {index + 1}
-          </Button>
+          </button>
         ))}
       </div>
-      {selectedSprint && (
-        <div className="sprint-info">
-          <div className="sprint-text">
-            <h2>{selectedSprint.title}</h2>
-            <p>
-              <strong>Focus:</strong> {selectedSprint.focus}
-            </p>
-            <p>
-              <strong>Goal:</strong> {selectedSprint.goal}
-            </p>
+      <div className="sprint-container">
+        {sprintData.map((sprint, index) => (
+          <div
+            onMouseEnter={() => setHoveredSprint(index)}
+            onMouseLeave={() => setHoveredSprint(null)}
+            key={index}
+            className={`sprint-info ${
+              hoveredSprint === index ? "highlight" : ""
+            }`}
+          >
+            <div className="sprint-content">
+              <div className="sprint-text">
+                <div className="sprint-title">{sprint.title}</div>
+                <div className="sprint-duration">{sprint.duration}</div>
+                <p className="focus">
+                  <strong>Focus:</strong> {sprint.focus}
+                </p>
+                <p className="focus">
+                  <strong>Goal:</strong> {sprint.goal}
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="sprint-icon">{selectedSprint.icon}</div>
-        </div>
-      )}
+        ))}
+      </div>
     </div>
   );
 };
