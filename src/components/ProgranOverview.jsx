8c4043c5cc000c70f155629a8c8d2069b7bc186e
Neha Singh <nehasngh56@gmail.com>
2025-04-11 14:49:29 +0530

    fixed the browser logo

---
+++ src/components/ProgranOverview.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/programoverview.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <nav className="sidebar">
+        <ul className="nav-list">
+          <li className="nav-item">Core Curriculum</li>
+          <li className="nav-item">Out Of Class Activities</li>
+          <li className="nav-item">Startup Internship</li>
+          <li className="nav-item">Build Your Own Venture</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
